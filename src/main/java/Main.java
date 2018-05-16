13c462873d8c1b34f27b37adc461283f171dc2e7
Emmanuel Meric de Bellefon <emmanuel.meric-de-bellefon@epita.fr>
2018-05-16 10:09:48 +0200

    canvas: manage to have a rectangle moving smoothly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
         MyFrame frame = new MyFrame();
     }
 }
--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -1,5 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MyFrame extends JFrame {
 
@@ -7,23 +13,57 @@ public class MyFrame extends JFrame {
 
     public MyFrame() {
         setTitle("MyRPGMaker");
-        setDefaultLookAndFeelDecorated(true);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
 
+
         panel = new MyPanel();
         setContentPane(panel);
+
+        // comment the line below to display the exit button
         //go();
 
-        JButton button = new JButton("Button");
+        /* Setting an exit button */
+        JButton button = new JButton("Quit");
+        button.addActionListener(actionEvent -> System.exit(0));
         Container pane = getContentPane();
         GroupLayout gl = new GroupLayout(pane);
         pane.setLayout(gl);
         gl.setAutoCreateContainerGaps(true);
         gl.setHorizontalGroup(gl.createSequentialGroup().addComponent(button));
         gl.setVerticalGroup(gl.createSequentialGroup().addComponent(button));
+
+
+        /* Setting icon */
+        /* Note : image took from internet with rights for utilisation */
+        ImageIcon icon = null;
+        try {
+            icon = new ImageIcon(ImageIO.read(new File("src/main/resources/img/icon.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setIconImage(icon.getImage());
+
+
+        /* Creating menus */
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
+
+        //Create the menu bar
+        menuBar = new JMenuBar();
+
+        //Build the first menu
+        menu = new JMenu("A menu");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+        setJMenuBar(menuBar);
 
     }
 
